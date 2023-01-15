f6e6a1a92d42965d4290c05925de3452ed0bc4e7
PoIarStar <bogdanmurzintsev@yandex.ru>
2023-01-15 16:11:00 +0300

    created start screen

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+import start
+import game
+import final
+
+
+def abort():
+    pygame.quit()
+
+
+def main():
+    pygame.init()
+    pygame.display.set_caption('Слабое звено')
+    size = 800, 400
+    screen = pygame.display.set_mode(size)
+    names = []
+    if not start.main(names, screen):
+        return abort()
+    pygame.quit()
+
+
+try:
+    if __name__ == '__main__':
+        main()
+except Exception as e:
+    print(e, e.args)
+    pygame.quit()
